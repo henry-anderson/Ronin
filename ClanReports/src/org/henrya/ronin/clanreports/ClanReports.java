d0ed52c3fd6f7b55f20aa7da3ee9806f7125de59
Henry Anderson <henryanderson1@hotmail.com>
2019-07-15 23:39:34 -0500

    Created ClanReports

---
+++ ClanReports/src/org/henrya/ronin/clanreports/ClanReports.java
@@ -0,0 +1,50 @@
+package org.henrya.ronin.clanreports;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.henrya.httpicnic.http.HttpConnectionException;
+
+/**
+ * Generates HTML reports about clan activity
+ * @author Henry Anderson
+ *
+ */
+public class ClanReports {
+	/* A month in milliseconds */
+	public static final long MONTH = 30 * 24 * 60 * 60 * 1000L;
+	
+	public static void main(String[] main) {
+		try {
+			WebDKPSession session = new WebDKPSession("https://www.webdkp.com/dkp/Reborn/Ronin");
+			System.out.println("Loading players...");
+			session.loadPlayers();
+			System.out.println("Loading awards...");
+			session.loadAwards();
+			System.out.println("Loading activity...");
+			session.loadActivity();
+			System.out.println("Creating report...");
+			HtmlTable html = new HtmlTable(session);
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+		    Date date = new Date(System.currentTimeMillis());
+		    String filename = "RoninReport_" + sdf.format(date) + ".html";
+		    File jarFile = new File(ClanReports.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+		    File file = new File(jarFile.getParent(), filename);
+		    if(file.exists()) {
+		    	file.delete();
+		    }
+		    file.createNewFile();
+		    BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+			bw.append(html.getHtmlFile());
+			bw.close();
+		    System.out.println("Report: " + file.getAbsolutePath() + " has been created");
+		} catch(IOException | HttpConnectionException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ClanReports/src/org/henrya/ronin/clanreports/HtmlTable.java
@@ -0,0 +1,75 @@
+package org.henrya.ronin.clanreports;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HtmlTable {
+	private WebDKPSession session;
+	private String html = "";
+	
+	public HtmlTable(WebDKPSession session) {
+		this.session = session;
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+	    Date date = new Date(System.currentTimeMillis());
+		this.html += "<html>"
+				+ "<head>\n"
+				+ "<script src='https://www.kryogenix.org/code/browser/sorttable/sorttable.js'></script>"
+				+ "<style>\n"
+				+ "table {font-family: Helvetica;border-collapse: collapse;width: 100%;}\n"
+				+ "td, th {border: 1px solid #dddddd;text-align: center;padding: 8px;}\n" 
+				+ "tr:nth-child(even) {background-color: #dddddd;}\n"
+				+ "</style>\n"
+				+ "</head>\n"
+				+ "<body style='margin:0'>\n"
+				+ "<br>\n"
+				+ "<center>\n"
+				+ "<b>" + sdf.format(date) + "</b>"
+				+ "<br>\n"
+				+ "Total players: <b>" + this.session.getPlayers().size() + "</b>\n"
+				+ "<br>\n"
+				+ "Active players: <b>" + this.session.getActiveCount() + "</b>\n"
+				+ "<br>\n";
+		
+		int totalMonthlyPoints = 0;
+		for(Player player : this.session.getPlayers()) {
+			totalMonthlyPoints += player.getPointsThisMonth();
+		}
+		
+		this.html += "Total points this month: <b>" + totalMonthlyPoints + "</b>\n"
+				+ "<br>\n"
+				+ "</center>\n"
+				+ "</br>\n"
+				+ "<table class='sortable'>\n"
+				+ "<tr>\n"
+				+ "<th>Index</th>\n"
+				+ "<th>Name</th>\n"
+				+ "<th>Total Points</th>\n"
+				+ "<th>Lifetime Points</th>\n"
+				+ "<th>Points This Month</th>\n"
+				+ "<th>Activity</th>\n"
+				+ "</tr>\n";
+		this.createTable();
+	}
+	
+	private void createTable() {
+		int index = 1;
+		for(Player player : this.session.getPlayers()) {
+			this.html += "<tr>\n"
+					+ "<td>" + index + "</td>\n"
+					+ "<td>" + player.getName() + "</td>\n"
+					+ "<td>" + player.getPoints() + "</td>\n"
+					+ "<td>" + player.getLifetimePoints() + "</td>\n"
+					+ "<td>" + player.getPointsThisMonth() + "</td>\n"
+					+ player.getActivity() + "\n"
+					+ "</tr>\n";
+			index++;
+		}
+		this.html += "</table>"
+				+ "</body>"
+				+ "</html>";
+	}
+	
+	public String getHtmlFile() {
+		return this.html;
+	}
+}
