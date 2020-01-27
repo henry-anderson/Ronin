513d0ea304408ef988d22f6d7b8d1887b3f1d016
Henry Anderson <henryanderson1@hotmail.com>
2020-01-26 22:42:14 -0600

    Alias bug fix

--- docs/count/playeraliases.js
+++ docs/count/playeraliases.js
@@ -7,20 +7,24 @@ function loadAliases(text) {
         let line = split[i];
 		let commaSplit = line.split(",");
 		let player = commaSplit[0].trim();
+		console.log("." + player + ".")
 		let aliases = line.replace(player + ",", "").split(",");
+		aliases = aliases.map(function (el) {
+			return el.trim().toLowerCase();
+		});
 		playerMap.set(player, aliases);
 	}
 }
 
 function getPlayerFromAlias(aliasInput) {
 	for (let [player, aliases] of playerMap) {
-		//let aliases = playerMap.get(player);
-		if(player == aliasInput) {
+		if(player.toLowerCase() == aliasInput.toLowerCase()) {
 			return player
 		}
 		for(let i = 0; i < aliases.length; i++) {
 			let alias = aliases[i];
-			if(aliasInput == alias) {
+			console.log("." + alias + ".");
+			if(aliasInput.toLowerCase() == alias.toLowerCase()) {
 				return player;
 			}
 		}
